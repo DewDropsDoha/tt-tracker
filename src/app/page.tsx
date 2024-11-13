4101843f15a3ee435a25bc8e6b6a809bcb13fd7e
DewDropsDoha <alihaiderdoha@gmail.com>
2024-11-13 23:50:22 +0600

    Initialize react nextjs app

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-5xl p-8 font-[family-name:var(--font-geist-sans)]">
+      <h1>TT Tracker</h1>
+    </div>
+  );
+}
