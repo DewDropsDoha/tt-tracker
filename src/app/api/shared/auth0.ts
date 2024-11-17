8724b9d1dfeae077095619c1c29319e94a6d7775
DewDropsDoha <alihaiderdoha@gmail.com>
2024-11-17 12:20:47 +0600

    Fix version mismatch issue

--- src/app/api/shared/auth0.ts
+++ src/app/api/shared/auth0.ts
@@ -1,12 +1,15 @@
 import { getAccessToken } from '@auth0/nextjs-auth0';
-import { jwtDecode } from 'jwt-decode';
+import { jwtDecode, JwtPayload } from 'jwt-decode';
+
+interface DecodedJwtPayload extends JwtPayload {
+  permissions: string[];
+}
 
 export const getUserPermissions = async () => {
   const { accessToken } = await getAccessToken();
 
   if (accessToken) {
-    const decodedToken = jwtDecode(accessToken);
-    // @ts-expect-error There's more in service.spreadsheets.values
+    const decodedToken = jwtDecode<DecodedJwtPayload>(accessToken);
     return decodedToken?.permissions || [];
   }
 
