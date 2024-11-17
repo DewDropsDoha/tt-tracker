2d85240ce99108edc3e8019fd5d069b00ae3ed8e
DewDropsDoha <alihaiderdoha@gmail.com>
2024-11-17 15:49:57 +0600

    Show rankings in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
 'use client';
 
-import Link from 'next/link';
-import Navbar from './components/Navbar/Navbar';
+import Rank from './rank/Rank';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <div className="flex flex-col items-center justify-center p-8">
-        <div className="text-5xl">Table Tennis Tracker</div>
-
-        <Link href="/match-tracker">
-          <button className="mt-6 px-6 py-3 bg-white text-black rounded-md">
-            Go to Match Tracker
-          </button>
-        </Link>
-      </div>
+      <Rank />
     </div>
   );
 }
