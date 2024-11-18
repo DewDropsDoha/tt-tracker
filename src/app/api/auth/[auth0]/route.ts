8dffb9c2c7539d004739ff5b612e2510a36ba6c5
DewDropsDoha <alihaiderdoha@gmail.com>
2024-11-18 22:14:41 +0600

    Commented custom login, using default handleAuth()

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,19 +1,19 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { handleAuth } from '@auth0/nextjs-auth0';
 
-// export const GET = await handleAuth();
+export const GET = await handleAuth();
 
-export const GET = await handleAuth({
-  login: handleLogin({
-    authorizationParams: {
-      audience: process.env.AUTH0_AUDIENCE,
-      redirect_uri: `${process.env.AUTH0_BASE_URL}/api/auth/callback`,
-    },
-    // returnTo: '/',
-  }),
-  //   signup: handleLogin({
-  //     authorizationParams: {
-  //       screen_hint: 'signup',
-  //     },
-  //     returnTo: '/',
-  //   }),
-});
+// export const GET = await handleAuth({
+//   login: handleLogin({
+//     authorizationParams: {
+//       audience: process.env.AUTH0_AUDIENCE,
+//       redirect_uri: `${process.env.AUTH0_BASE_URL}/api/auth/callback`,
+//     },
+//     // returnTo: '/',
+//   }),
+//   signup: handleLogin({
+//     authorizationParams: {
+//       screen_hint: 'signup',
+//     },
+//     returnTo: '/',
+//   }),
+// });
