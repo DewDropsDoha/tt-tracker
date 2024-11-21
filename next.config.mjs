fde678d59539d56b8a2a282db0c9789a6998730f
DewDropsDoha <alihaiderdoha@gmail.com>
2024-11-21 16:41:15 +0600

    Disable caching globally by setting Cache-Control headers

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@
  */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
